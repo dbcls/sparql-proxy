f5575eb4b41eb41e1628cf11811bac91dcff1e59
Keita Urashima <ursm@ursm.jp>
2016-02-04 14:22:59 +0900

    Cosmetic

--- src/server.js
+++ src/server.js
@@ -1,50 +1,50 @@
-import express from 'express'
-import { Parser as SparqlParser } from 'sparqljs'
-import Job from './job'
-import SocketIo from 'socket.io'
-import Tracker from './tracker'
-import http from 'http'
+import express from 'express';
+import { Parser as SparqlParser } from 'sparqljs';
+import Job from './job';
+import SocketIo from 'socket.io';
+import Tracker from './tracker';
+import http from 'http';
 
-var app = express();
-var server = http.Server(app);
-var io = SocketIo(server);
+const app = express();
+const server = http.Server(app);
+const io = SocketIo(server);
 
-var port = process.env.PORT || 3000;
-var backend = process.env.SPARQL_BACKEND;
-var tracker = new Tracker();
+const port = process.env.PORT || 3000;
+const backend = process.env.SPARQL_BACKEND;
+const tracker = new Tracker();
 
 app.use(express.static('public'));
 
-app.get('/sparql', function (req, res) {
-  var query = req.query.query;
-  var parser = new SparqlParser();
-  var parsedQuery;
-  var format = req.query.format;
+app.get('/sparql', (req, res) => {
+  const query = req.query.query;
+  const parser = new SparqlParser();
+  let parsedQuery;
+  const format = req.query.format;
 
   try {
     parsedQuery = parser.parse(query);
-  } catch(ex) {
+  } catch (ex) {
     console.log(ex);
-    res.status(400).send("Query parse failed");
+    res.status(400).send('Query parse failed');
     return;
   }
 
-  var queryType = parsedQuery.queryType;
-  if (parsedQuery.type != "query" || queryType != "SELECT") {
-    console.log("Query type not allowed: " + parsedQuery.type + "(" + queryType + ")");
-    res.status(400).send("Query type not allowed");
+  const queryType = parsedQuery.queryType;
+  if (parsedQuery.type !== 'query' || queryType !== 'SELECT') {
+    console.log(`Query type not allowed: ${parsedQuery.type} (${queryType})`);
+    res.status(400).send('Query type not allowed');
     return;
   }
 
-  var accept = req.header.accept || 'application/sparql-results+json';
-  let job = new Job(backend, query, accept);
-  let promise = tracker.enqueue(job);
+  const accept = req.header.accept || 'application/sparql-results+json';
+  const job = new Job(backend, query, accept);
+  const promise = tracker.enqueue(job);
 
   promise.then((result) => {
     res.send(result);
     return result;
   }).catch((error) => {
-    res.status(500).send("ERROR");
+    res.status(500).send('ERROR');
   });
 });
 
@@ -68,7 +68,7 @@ tracker.on('state', (state) => {
   io.emit('state', state);
 });
 
-server.listen(port, function () {
-  var port = server.address().port;
+server.listen(port, () => {
+  const port = server.address().port;
   console.log('sparql-proxy listening at', port);
 });
