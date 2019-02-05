07286f8a2e228e7af907cf6399e4d8de5027d35a
Yoji Shidara <dara@shidara.net>
2019-02-05 12:46:04 +0900

    Simplify

--- client/app.jsx
+++ client/app.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/scss/bootstrap.scss';
 import './app.scss';
 import 'font-awesome/css/font-awesome.css';
-import uuid from 'uuid';
 import queryString from 'query-string';
 import 'babel-regenerator-runtime';
 
@@ -17,83 +16,8 @@ class Navbar extends React.Component {
   render() {
     return (
       <nav className="navbar fixed-top navbar-dark bg-dark">
-        <a className="navbar-brand" href="#">SPARQL Proxy</a>
+        <span className="navbar-brand fg-dark">SPARQL Proxy</span>
       </nav>
-    );
-  }
-}
-
-class StatusLabel extends React.Component {
-  render() {
-    const n = Math.floor(this.props.response.statusCode / 100);
-    const c = { 2: 'success', 3: 'info', 4: 'danger', 5: 'danger' }[n] || 'default';
-
-    return (
-      <span className={"label label-" + c}>{this.props.response.statusText}</span>
-    );
-  }
-}
-
-class ResponseBox extends React.Component {
-  render() {
-    let table = "";
-    if (!this.props.response.error) {
-      const json = JSON.parse(this.props.response.data);
-      if (this.props.response.ok && json.results && json.results.bindings) {
-        table = <SparqlResultTable json={json} />;
-      }
-    }
-    return (
-      <div className="card card-body my-3">
-        <h4 className="card-title">Response <StatusLabel response={this.props.response} /></h4>
-        {this.error()}
-        {table}
-        <textarea className="form-control" rows="10" value={this.props.response.data} readOnly />
-      </div>
-    );
-  }
-
-  error() {
-    if (this.props.response.error) {
-      return (
-        <div className="alert alert-danger">{this.props.response.error}</div>
-      );
-    } else {
-      return '';
-    }
-  }
-}
-
-class SparqlResultTable extends React.Component {
-  render() {
-    const head = this.props.json.head.vars;
-    const data = this.props.json.results.bindings;
-
-    const ths = head.map((col) => {
-      return (
-        <th>{col}</th>
-      );
-    });
-
-    const trs = data.map((row) => {
-      const tds = head.map((c) => {
-        const value = row[c] ? row[c].value : "";
-        return (
-          <td>{value}</td>
-        );
-      });
-      return (
-        <tr>{tds}</tr>
-      );
-    });
-
-    return (
-      <table className="table sparql-results">
-        <thead><tr>{ths}</tr></thead>
-        <tbody>
-          {trs}
-        </tbody>
-      </table>
     );
   }
 }
@@ -125,52 +49,6 @@ class Editor extends React.Component {
 
     this.codeMirror = CodeMirror.fromTextArea(this.textareaNode, options);
   }
-
-  codemirrorValueChanged(doc, change) {
-    if (this.props.onChange && change.origin !== 'setValue') {
-      this.props.onChange(doc.getValue(), change);
-    }
-  }
-}
-
-class RequestBox extends React.Component {
-  constructor() {
-    super(...arguments);
-    this.state = { query: this.props.query };
-  }
-
-  render() {
-    let requestStatus = "";
-    if (this.props.running) {
-      requestStatus = (
-        <span>
-          <span className="running-icon fa fa-refresh fa-spin"></span>
-          <span>{this.props.request.jobState}</span>
-        </span>
-      );
-    }
-    return (
-      <div className="card card-body my-3">
-        <h4 className="card-title">Query</h4>
-        <form>
-          <div className="form-group">
-            <Editor />
-          </div>
-        </form>
-      </div>
-    );
-  }
-
-  handleQueryChange(query) {
-    this.setState({ query });
-  }
-
-  handleSubmit(e) {
-    if (e) {
-      e.preventDefault();
-    }
-    this.props.onSubmit(this.state.query);
-  }
 }
 
 class QueryBox extends React.Component {
@@ -180,54 +58,15 @@ class QueryBox extends React.Component {
   }
 
   render() {
-    const res = this.state.response ? <ResponseBox response={this.state.response} /> : "";
     return (
       <div className="container-fluid">
-        <RequestBox query={this.props.query} onSubmit={this.handleSubmit.bind(this)} request={this.state.request} running={this.state.running} />
-        {res}
+        <form action="">
+          <div className="form-group">
+            <Editor />
+          </div>
+        </form>
       </div>
     );
-  }
-
-  async handleSubmit(query) {
-    this.setState({ response: null, request: { jobState: null }, running: true });
-    const token = uuid.v4();
-
-    const timerId = setInterval(async () => {
-      const job = await fetch(`./jobs/${token}`);
-      const { state } = await job.json();
-      this.setState({ request: { jobState: state } });
-    }, 1000);
-
-    try {
-      const params = queryString.stringify({ query, token });
-      const response = await fetch(`./sparql?${params}`, {
-        headers: {
-          'accept': 'application/sparql-results+json'
-        }
-      });
-      const statusText = `${response.status} ${response.statusText}`;
-      const statusCode = response.status;
-      this.setState({ request: null, running: false });
-
-      if (response.ok) {
-        const data = await response.text();
-        this.setState({ response: { statusText, statusCode, data, ok: true } });
-      } else {
-        if (response.headers.get('content-type').indexOf('application/json') !== -1) {
-          const { message, data } = await response.json();
-          this.setState({ response: { statusText, statusCode, error: message, data } });
-        } else {
-          const error = await response.text();
-          this.setState({ response: { statusText, statusCode, error } });
-        }
-      }
-    } catch (err) {
-      console.log('failed', err);
-      this.setState({ response: { error: err.message }, request: { jobState: null }, running: false });
-    } finally {
-      clearInterval(timerId);
-    }
   }
 }
 
