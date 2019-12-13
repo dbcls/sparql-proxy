c878c89b31b0911204ffe26ac8cd3df6cd79ed32
Keita Urashima <ursm@ursm.jp>
2019-12-13 16:04:34 +0900

    Cosmetic

--- tests/mock-server/server.rb
+++ tests/mock-server/server.rb
@@ -6,15 +6,15 @@ require 'sinatra'
 require 'sinatra/sparql'
 
 repository = RDF::Repository.new do |graph|
-  graph << [RDF::IRI.new("http://example.com"), RDF::Vocab::DC.title, "Hello, world!"]
+  graph << [RDF::IRI.new('http://example.com'), RDF::Vocab::DC.title, 'Hello, world!']
 end
 
 get '/sparql' do
-    query = ::URI.decode(params["query"].to_s)
-    SPARQL.execute(query, repository)
+  query = ::URI.decode(params['query'].to_s)
+  SPARQL.execute(query, repository)
 end
 
 post '/sparql' do
-    query = ::URI.decode(params["query"].to_s)
-    SPARQL.execute(query, repository)
+  query = ::URI.decode(params['query'].to_s)
+  SPARQL.execute(query, repository)
 end
