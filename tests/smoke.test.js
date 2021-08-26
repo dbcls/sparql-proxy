0a17ff3e4197e7beb841b6c79078792b859bbd02
Yoji Shidara <dara@shidara.net>
2021-08-26 10:28:34 +0900

    Pritter

--- tests/smoke.test.js
+++ tests/smoke.test.js
@@ -1,36 +1,37 @@
-import fetch from 'node-fetch';
-import { spawn } from 'child_process';
+import fetch from "node-fetch";
+import { spawn } from "child_process";
 
 async function runProxy(env, cb) {
   const port = 9999;
 
   const proxyProcess = await new Promise((resolve, reject) => {
-    const ps = spawn(
-      'node',
-      ['--experimental-modules', 'src/server.mjs'],
-      {
-        env: Object.assign({}, {
+    const ps = spawn("node", ["--experimental-modules", "src/server.mjs"], {
+      env: Object.assign(
+        {},
+        {
           SPARQL_BACKEND: `http://localhost:${backendPort}/sparql`,
-          PORT: port
-        }, env, process.env)
-      }
-    );
-
-    ps.on('exit', () => {
-      reject('unexpected sparql-proxy exit');
+          PORT: port,
+        },
+        env,
+        process.env
+      ),
     });
 
-    ps.on('error', code => {
+    ps.on("exit", () => {
+      reject("unexpected sparql-proxy exit");
+    });
+
+    ps.on("error", (code) => {
       reject(`sparql-proxy error (code ${code})`);
     });
 
-    ps.stdout.on('data', data => {
-      if (data.toString().includes('sparql-proxy listening at')) {
+    ps.stdout.on("data", (data) => {
+      if (data.toString().includes("sparql-proxy listening at")) {
         resolve(ps);
       }
     });
 
-    ps.stderr.on('data', data => {
+    ps.stderr.on("data", (data) => {
       console.error(data.toString());
     });
   });
@@ -40,12 +41,12 @@ async function runProxy(env, cb) {
   try {
     await cb({
       root,
-      endpoint: new URL('/sparql', root)
+      endpoint: new URL("/sparql", root),
     });
   } finally {
     await new Promise((resolve) => {
-      proxyProcess.removeAllListeners('exit');
-      proxyProcess.on('exit', resolve);
+      proxyProcess.removeAllListeners("exit");
+      proxyProcess.on("exit", resolve);
       proxyProcess.kill();
     });
   }
@@ -57,25 +58,27 @@ let backendProcess;
 
 beforeAll(async () => {
   backendProcess = await new Promise((resolve, reject) => {
-    const ps = spawn(
-      'npx',
-      [
-        'comunica-sparql-file-http',
-        '-p', backendPort,
-        'tests/fixtures/hello.ttl'
-      ]
-    );
+    const ps = spawn("npx", [
+      "comunica-sparql-file-http",
+      "-p",
+      backendPort,
+      "tests/fixtures/hello.ttl",
+    ]);
 
-    ps.on('exit', () => {
-      reject('unexpected endpoint exit');
+    ps.on("exit", () => {
+      reject("unexpected endpoint exit");
     });
 
-    ps.on('error', code => {
+    ps.on("error", (code) => {
       reject(`endpoint error (code ${code})`);
     });
 
-    ps.stderr.on('data', data => {
-      if (data.toString().includes(`Server running on http://localhost:${backendPort}/sparql`)) {
+    ps.stderr.on("data", (data) => {
+      if (
+        data
+          .toString()
+          .includes(`Server running on http://localhost:${backendPort}/sparql`)
+      ) {
         resolve(ps);
       }
     });
@@ -89,55 +92,55 @@ afterAll(async () => {
       return;
     }
 
-    backendProcess.removeAllListeners('exit');
-    backendProcess.on('exit', resolve);
+    backendProcess.removeAllListeners("exit");
+    backendProcess.on("exit", resolve);
     backendProcess.kill();
   });
 });
 
-test('GET / should redirect to /sparql', async () => {
-  await runProxy({}, async ({root, endpoint}) => {
+test("GET / should redirect to /sparql", async () => {
+  await runProxy({}, async ({ root, endpoint }) => {
     const res = await fetch(root, {
-      redirect: 'manual'
+      redirect: "manual",
     });
 
     expect(res.status).toEqual(302);
-    expect(res.headers.get('Location')).toEqual(endpoint.toString());
+    expect(res.headers.get("Location")).toEqual(endpoint.toString());
   });
 });
 
 test.each([
-  {CACHE_STORE: null},
-  {CACHE_STORE: 'file', CACHE_STORE_PATH: `tmp/test-cache-${process.pid}`},
-  {CACHE_STORE: 'memory'},
-  {CACHE_STORE: 'redis'},
-  {CACHE_STORE: 'memcache'},
-  {COMPRESSOR: 'snappy'},
-])('GET /sparql (env: %p)', async (env) => {
-  await runProxy(env, async ({endpoint}) => {
-    endpoint.searchParams.set('query', 'SELECT * { ?s ?p ?o. }');
+  { CACHE_STORE: null },
+  { CACHE_STORE: "file", CACHE_STORE_PATH: `tmp/test-cache-${process.pid}` },
+  { CACHE_STORE: "memory" },
+  { CACHE_STORE: "redis" },
+  { CACHE_STORE: "memcache" },
+  { COMPRESSOR: "snappy" },
+])("GET /sparql (env: %p)", async (env) => {
+  await runProxy(env, async ({ endpoint }) => {
+    endpoint.searchParams.set("query", "SELECT * { ?s ?p ?o. }");
 
     const res = await fetch(endpoint, {
       headers: {
-        Accept: 'application/sparql-results+json'
-      }
+        Accept: "application/sparql-results+json",
+      },
     });
 
     expect(res.status).toEqual(200);
 
     expect(await res.json()).toEqual({
       head: {
-        vars: ['s', 'p', 'o']
+        vars: ["s", "p", "o"],
       },
       results: {
         bindings: [
           {
-            s: {type: 'uri',     value: 'http://example.com'},
-            p: {type: 'uri',     value: 'http://purl.org/dc/terms/title'},
-            o: {type: 'literal', value: 'Hello, world!'}
-          }
-        ]
-      }
+            s: { type: "uri", value: "http://example.com" },
+            p: { type: "uri", value: "http://purl.org/dc/terms/title" },
+            o: { type: "literal", value: "Hello, world!" },
+          },
+        ],
+      },
     });
   });
 });
