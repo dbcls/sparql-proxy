50536a37b397f2734e1b6eebb9a8037c1465066f
Yoji Shidara <dara@shidara.net>
2024-02-08 21:27:20 +0900

    Add replace-prefix plugin

--- plugins/replace-prefix/README.md
+++ plugins/replace-prefix/README.md
@@ -1,1 +1,8 @@
 # Replace Prefix Plugin
+
+This plugin replaces the prefix of a string with another prefix.
+This rewrites both the request and response.
+
+Give mappings as `mappings.tsv` file in the following format:
+
+"prefix used for clients" TAB "prefix used for backend"
--- plugins/replace-prefix/main.ts
+++ plugins/replace-prefix/main.ts
@@ -1,9 +1,116 @@
+import fs from "node:fs/promises";
+import path from "node:path";
+
 import { type Context, Response } from "../../src/plugins";
+import { type SparqlQuery } from "sparqljs";
+
+function replaceNamedNode(
+  obj: any,
+  replacer: (before: string) => string,
+): SparqlQuery {
+  for (const key in obj) {
+    const value = obj[key];
+    if (typeof value === "object") {
+      obj[key] = replaceNamedNode(value, replacer);
+    } else {
+      const isReplaceTarget = obj.termType === "NamedNode" && key === "value";
+      obj[key] = isReplaceTarget ? replacer(value) : value;
+    }
+  }
+  return obj;
+}
+
+function replacePrefix(
+  iri: string,
+  mappings: [string, string][],
+  reverse: boolean,
+): string {
+  for (const [a, b] of mappings) {
+    const from = reverse ? b : a;
+    const to = reverse ? a : b;
+
+    if (iri.startsWith(from)) {
+      return to + iri.slice(from.length);
+    }
+  }
+  return iri;
+}
+
+function rewriteQuery(
+  query: SparqlQuery,
+  replacer: (before: string) => string,
+): SparqlQuery {
+  return replaceNamedNode(query, replacer);
+}
+
+function rewriteResponse(response, replacer: (before: string) => string) {
+  for (const binding of response.body.results.bindings) {
+    for (const key in binding) {
+      const value = binding[key];
+      if (value.type === "uri") {
+        binding[key].value = replacer(value.value);
+      }
+    }
+  }
+  return response;
+}
+
+type Mappings = [string, string][];
+
+async function loadMappingsFromTSV(tsvPath: string): Promise<Mappings> {
+  const mappings: Mappings = [];
+  const tsv = await fs.readFile(tsvPath, "utf-8");
+  const lines = tsv.split("\n");
+  for (const line of lines) {
+    if (line === "") continue;
+    const [from, to] = line.split("\t");
+    mappings.push([from, to]);
+  }
+  return mappings;
+}
+
+let mappings: Mappings = [];
+
+async function loadMappings() {
+  const filename = "mappings.tsv";
+  const __dirname = (import.meta as any).dirname;
+  const resolvedTsvPath = path.resolve(__dirname, filename);
+
+  if (!(await fs.stat(resolvedTsvPath).catch((e) => false))) {
+    console.log(
+      `[replace-prefix] Mappings file {${resolvedTsvPath} is not found, no mappings will be applied.`,
+    );
+    return;
+  }
+
+  console.info(`[replace-prefix] Loading mappings from ${resolvedTsvPath}`);
+  mappings = await loadMappingsFromTSV(resolvedTsvPath);
+}
+
+await loadMappings();
 
 export default async function rewritePrefix(
   ctx: Context,
-  next
+  next: () => Response,
 ): Promise<Response> {
-  // TODO
-  return await next();
+  // rewrite query
+  rewriteQuery(ctx.query, (before) => {
+    const after = replacePrefix(before, mappings, false);
+    if (before !== after) {
+      console.log("[replace-prefix] REPLACE (QUERY)", before, "-->", after);
+    }
+    return after;
+  });
+
+  // issue request
+  const resp = await next();
+
+  // rewrite response
+  return rewriteResponse(resp, (before) => {
+    const after = replacePrefix(before, mappings, true);
+    if (before !== after) {
+      console.log("[replace-prefix] REPLACE (RESPONSE)", before, "-->", after);
+    }
+    return after;
+  });
 }
